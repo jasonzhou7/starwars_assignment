dd8259cf39b53cfb09431c98fcd61440bc69a8dc
jasonzhou7 <jzhou145@my.bcit.ca>
2021-10-24 13:28:21 -0700

    added constants.js

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  {{ info }} 
   <MovieList/>
 </template>
 
 <script>
 import MovieList from './components/MovieList.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      info: []
+    }
+  },
   components: {
     MovieList
-  }
+  },
+  mounted () {
+    axios
+    .get('https://swapi.dev/api/')
+    .then(response => {
+      this.info = response.data;
+      console.log(this.info)})
+    .catch(error => {
+      console.log(error)
+    })
+      }
 }
 </script>
 
--- src/utils/StarWarsAPI.js
+++ src/utils/StarWarsAPI.js
@@ -1,1 +1,10 @@
 import axios from 'axios'
+import { BASE_URL } from './Constants'
+
+export default axios.create({
+    baseURL: BASE_URL,
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json'
+      }
+})
