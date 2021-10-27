835a3652b597cd93618b937babc911995e533e2e
jasonzhou7 <jzhou145@my.bcit.ca>
2021-10-27 03:47:50 -0700

    added viewable list in modal

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,27 +1,18 @@
 <template>
-<!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" :data-bs-target="'#demo' + indexNum">
   {{ name }}
-  :data-bs-target="`#modal-` + name"
-  :id="`modal-` + name"
-  
 </button>
 
 <!-- Modal -->
 <div class="modal fade" :id="'demo' + indexNum" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <!-- <div class="modal-header"> -->
-        <!-- <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
-      <!-- </div> -->
-      <div v-for="(characters, index) in allCharacters" :key="index" class="modal-body">
-        {{ characters.name}}
-      </div>
-      <!-- <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div> -->
+
+
+        <ul>
+          <h2>Characters</h2>
+        <li v-for="(characters, index) in allCharacters" :key="index">{{characters.name}}</li>
+        </ul>
     </div>
   </div>
 </div>
@@ -104,7 +95,7 @@ export default {
 
 <style>
 .collapsible-wrapper {
-  align-items:center;
+  align-items:right;
   width: 50%;
   height: auto;
   border-radius: 10px;
